476b04d89daf923ca0565fe1201d88e0f9b9c9c0
Aaron <awitthohn@yahoo.com>
2023-08-29 16:39:52 -0400

    routes working basic skeleton functional

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React from 'react';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import MainContent from "./components/content";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/navbar"
+import About from "./components/about"
 
 function App() {
   return (
-    <div id="full-screen">
-      <Header />
-        <div className="background-color">
-          <MainContent/>
-        </div>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Navbar /> {/* Placed outside of the <main> */}
+        <main>
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <main>
+      <h2>About Me</h2>
+      <p>I have red hair</p>
+    </main>
+  );
+}
+
+export default About;
